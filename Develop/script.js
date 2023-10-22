4244ad0cf4cd67ea953a4f113dc83a2fc19d8380
Sarahy <sarahym0703@gmail.com>
2023-10-22 14:51:11 -0400

    added functions to the javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function getRandomNumber(max) {
+  var randomNumber=Math.random() * (max+1);
+  return Math.floor(randomNumber);
+}
+
+function getRandomElementFromArray(array) {
+  var randomPosition=getRandomNumber(array.length-1);
+  return array[randomPosition];
+} 
+
+function generatePassword() {
+  var password="Hello Password";
+  //
+
+  return password;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
